800c5acc8fa0843b64134ac3fcb4d4ebb0468e1a
Steven <steve0183@gmail.com>
2019-02-25 15:25:07 -0700

    need to fix question number at 14

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
     
     //Place your instance variables here
     
+    let allQuestions = QuestionBank()
+    var pickedAnswer : Bool = false
+    var questionNumber : Int = 0
+    var score : Int = 0
+    
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -21,31 +26,85 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        nextQuestion()
+        
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
+        
+        if sender.tag == 1 {
+            pickedAnswer = true
+        }
+        else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
+        
+        questionNumber += 1
+        
+        nextQuestion()
   
     }
     
     
     func updateUI() {
-      
+        
+        scoreLabel.text = "Score: \(score)"
+        progressLabel.text = "\(questionNumber + 1) / 13"
+        progressBar.frame.size.width = (view.frame.size.width / 13) * CGFloat(questionNumber + 1)
     }
     
 
     func nextQuestion() {
         
+        if questionNumber <= 12 {
+            
+            questionLabel.text = allQuestions.list[questionNumber].questionText
+            
+        }
+        else {
+
+            let alert = UIAlertController(title: "End of Quiz", message: "You have reached the end of the quiz.\n Would you like to start over", preferredStyle: .alert)
+            
+            let restartAction = UIAlertAction(title: "Restart", style: .default, handler: {
+                (UIAlertAction) in
+                
+                self.startOver()
+                
+                })
+            alert.addAction(restartAction)
+            
+            present(alert, animated: true, completion: nil)
+            
+        }
+        
+        updateUI()
+
     }
     
     
     func checkAnswer() {
         
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        
+        if correctAnswer == pickedAnswer {
+            score += 1
+            print("you\'ve got it")
+        }
+        else {
+            print("wrong")
+        }
+        
     }
     
     
     func startOver() {
-       
+        
+       questionNumber = 0
+       score = 0
+       nextQuestion()
     }
     
 
